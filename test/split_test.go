b46900617af6d7d9093c02a7b97f75c5e1e37f12
zhangxuecai <zhangxuecai@netbank.cn>
2021-05-07 11:54:35 +0800

    fix

--- test/split_test.go
+++ test/split_test.go
@@ -13,10 +13,26 @@ func TestA(t *testing.T) {
 	}
 }
 
-func TestB(t *testing.T) {
-	got := Split("a:b:c", ":")
-	want := []string{"a", "b", "c"}
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("expect:%v, got: %v", want, got)
+func TestGroup(t *testing.T) {
+	type testType struct {
+		input string
+		sep   string
+		want  []string
+	}
+
+	tests := map[string]testType{
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong seq":   {input: "a:b:c", sep: ",", want: []string{"a", "b", "c"}},
+		"more seq":    {input: "abcd", sep: "bc", want: []string{"a", "b", "c"}},
+		"leading seq": {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := Split(tc.input, tc.sep)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("expect:%v, got: %v", tc.want, got)
+			}
+		})
 	}
 }
